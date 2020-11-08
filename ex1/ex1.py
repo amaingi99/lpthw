c336bf12a32937600b1a7d3aa07fd8cd7485b199
amaingi <amaingi@cisco.com>
2020-11-08 16:23:24 +0530

    Ex1 + Study Drills

---
+++ ex1/ex1.py
@@ -0,0 +1,11 @@
+# Study Drill 2
+#print("Hello World")
+#print("Hello Again")
+#print("I like typing this.")
+#print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+
+# Study Drill 1
+print("Making my script print another line.")
