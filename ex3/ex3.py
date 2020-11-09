d318fdfbb48139cdce8f426ba08e931334461dd3
amaingi <amaingi@cisco.com>
2020-11-09 14:48:28 +0530

    Ex3 + study drills

---
+++ ex3/ex3.py
@@ -0,0 +1,32 @@
+# Print out the line
+print("I will now count my chickens:")
+
+# Calculate no of hens, addition and division operations: 25+5.0=30.0
+print("Hens", float(25+30/6))
+# BODMAS and left-to-right rule: 100-((25*3)%4)= 100-(75%4)=100-3=97
+print("Roosters", float(100-25*3%4))
+# Print out the line
+print("Now I will count the eggs:")
+
+# 1+(4%2)-0.25+6=1+0-0.25+6=6+0.75=6.75
+print(float(3+2+1-5+4%2-1/4+6))
+# Print out the line
+print("Is it true that 3+2<5-7?")
+# Comparison operator: 5<-2: False
+print(3+2<5-7)
+
+# 3+2 = 5
+print("What is 3+2?", 3+2)
+# 5-7 = -2
+print("What is 5-7?", 5-7)
+# Print out the line
+print("Oh, that's why it's False.")
+# Print out the line
+print("How about some more.")
+
+# 5>-2 True
+print("Is it greater?", 5>-2)
+# 5>=-2 True
+print("Is it greater or equal?", 5>=-2)
+# 5<=-2 False
+print("Is it less or equal?", 5<=-2)
---
+++ ex3/ex3_sd3.py
@@ -0,0 +1,14 @@
+# Study Drill 3
+
+a = 0
+b = 8
+c = 7
+
+# a-=b+=9 -> Invalid Syntax
+
+print(b**2)
+print(a==c)
+a+=b
+print(a)
+print(a | c)
+print(a&c)
